d9b64e3c3c87311f80657395ad411adf98ddda6c
VanArts <teacher@vanarts.com>
2016-08-17 11:09:36 -0700

    Slippy Map - Basic map with a VanArts pin

--- src/js/10/basicMap.js
+++ src/js/10/basicMap.js
@@ -3,14 +3,17 @@ const basicMap = () => {
   const vanarts = {
     latitude: 49.282703,
     longitude: -123.115371,
+    pin: null,
   };
   const map = new mxn.Mapstraction('map', 'googlev3');
   const latlon = new mxn.LatLonPoint(vanarts.latitude, vanarts.longitude);
+  vanarts.pin = new mxn.Marker(latlon);
 
   // zoom level large is street level/closer 21
   // zoom level small is outer space/father 0
   map.setCenterAndZoom(latlon, 18);
-
+  map.addLargeControls();
+  map.addMarker(vanarts.pin);
 };
 
 // If Node.js then export as public
