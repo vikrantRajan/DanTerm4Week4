4f8ed5201fb38ae560187e8022acf090e047764f
VanArts <teacher@vanarts.com>
2016-08-24 11:35:58 -0700

    MongoDb - After beer collection is created, insert many
    https://github.com/motowilliams/MontanaBreweries/blob/master/list.geojson

---
+++ mongo-beer-setup.js
@@ -0,0 +1,213 @@
+db.beer.insertMany(
+    [
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Bayern Brewery Inc.",
+                "Url": "http://www.bayernbrewery.com/",
+                "Opened": 1987,
+                "Country": "US",
+                "Zip": 59801,
+                "State": "Mt",
+                "City": "Missoula"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -114.020046,
+                    46.872604
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Kettle House Brewing Company",
+                "Url": "http://kettlehouse.com/",
+                "Opened": 1991,
+                "Country": "US",
+                "Zip": 59801,
+                "State": "Mt",
+                "City": "Missoula"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -113.998829,
+                    46.865539
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Big Sky Brewing Co.",
+                "Url": "http://www.bigskybrew.com/",
+                "Opened": 1995,
+                "Country": "US",
+                "Zip": 59808,
+                "State": "Mt",
+                "City": "Missoula"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -114.072504,
+                    46.92208
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Lolo Peak Brewing Company",
+                "Url": "http://lolopeakbrewery.com/",
+                "Opened": 2014,
+                "Country": "US",
+                "Zip": 59847,
+                "State": "Mt",
+                "City": "Lolo"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -114.082850,
+                    46.766007
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Draught Works",
+                "Url": "http://www.draughtworksbrewery.com/",
+                "Opened": 2011,
+                "Country": "US",
+                "Zip": 59802,
+                "State": "Mt",
+                "City": "Missoula"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -114.00358,
+                    46.877901
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Bitter Root Brewing",
+                "Url": "http://www.bitterrootbrewing.com/",
+                "Opened": 2009,
+                "Country": "US",
+                "Zip": 59840,
+                "State": "Mt",
+                "City": "Hamilton"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -114.153926,
+                    46.247757
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Kalispell Brewing Company",
+                "Url": "http://www.kalispellbrewing.com/",
+                "Opened": 2012,
+                "Country": "US",
+                "Zip": 59901,
+                "State": "Mt",
+                "City": "Kalispell"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -114.3126086,
+                    48.1944623
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Blackfoot River Brewing Co",
+                "Url": "http://www.blackfootriverbrewing.com/",
+                "Opened": 1993,
+                "Country": "US",
+                "Zip": 59601,
+                "State": "Mt",
+                "City": "Helena"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -112.042,
+                    46.586
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Flathead Lake Brewing Company",
+                "Url": "http://flatheadlakebrewing.com/",
+                "Opened": 2004,
+                "Country": "US",
+                "Zip": 59911,
+                "State": "Mt",
+                "City": "Big Fork"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -114.0457586,
+                    47.9979957
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Beaverhead Brewing Company",
+                "Url": "http://beaverheadbeer.com/",
+                "Opened": 2014,
+                "Country": "US",
+                "Zip": 59725,
+                "State": "Mt",
+                "City": "Dillon"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -112.6394679,
+                    45.2159599
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Bozeman Brewing Company",
+                "Url": "http://bozemanbrewing.com/",
+                "Opened": 2001,
+                "Country": "US",
+                "Zip": 59725,
+                "State": "Mt",
+                "City": "Bozeman"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.0237397,
+                    45.6846013
+                ]
+            }
+        }
+    ]
+)
