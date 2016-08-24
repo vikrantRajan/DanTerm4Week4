cafaa90726aa1236d34554610e5e34f1cd627451
VanArts <teacher@vanarts.com>
2016-08-24 11:49:57 -0700

    MongoDb - Select beer by zip

--- mongo.js
+++ mongo.js
@@ -3,10 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/test';
 // Connect using MongoClient
 MongoClient.connect(url, (connectError, db) => {
-  // Create a collection we want to drop later
-  const col = db.collection('classroom');
-  // Show that duplicate records got dropped
-  col.find({}).toArray((error, items) => {
+  const col = db.collection('beer');
+  col.find({ 'properties.Zip': 59801 }).toArray((error, items) => {
     console.log(items);
     db.close();
   });
