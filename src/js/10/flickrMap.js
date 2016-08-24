3557511e1f5523f9e0df49518eaae63e367ce2c7
VanArts <teacher@vanarts.com>
2016-08-24 09:43:08 -0700

    Flickr Map - Flickr photo paths plot on map with VanArts centre

--- src/js/10/flickrMap.js
+++ src/js/10/flickrMap.js
@@ -14,20 +14,21 @@ const flickrMap = () => {
   const map = new mxn.Mapstraction('map', 'googlev3');
 
   getFlickrData((response) => {
-    console.log(response);
+    $.each(response.photos, (index, photo) => {
+      const latlon = new mxn.LatLonPoint(photo.latitude, photo.longitude);
+
+      const pin = new mxn.Marker(latlon);
+      pin.setInfoBubble(photo.path);
+      map.addMarker(pin);
+    });
   });
+
   const vanarts = {
     latitude: 49.282703,
     longitude: -123.115371,
     pin: null,
   };
-
   const latlon = new mxn.LatLonPoint(vanarts.latitude, vanarts.longitude);
-
-  vanarts.pin = new mxn.Marker(latlon);
-  vanarts.pin.setInfoBubble('My School');
-  map.addMarker(vanarts.pin);
-
 
   map.setCenterAndZoom(latlon, 18);
   map.addLargeControls();
