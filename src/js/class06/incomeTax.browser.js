ef7ce75101b4ae763bdc38189957995d01e03843
Dan BROOKS <githubcom@domaindesign.ca>
2016-10-12 10:58:03 -0700

    Exercise: Sales tax last step

---
+++ src/js/class06/incomeTax.browser.js
@@ -0,0 +1,42 @@
+/* global calculateTax, currencyFormat, utils */
+/* steps
+    1) gather input
+        input field with design and UI
+        submit button/ field event(change) to trigger our code
+        validate, ensure number
+    2) allow input refinement and output result
+    3) determine tax range
+        range constant? Yes, not flexible
+        multiply percentages
+        store for output (tax owed)
+    4) Apply currency formatting (with Canadian dollar sign and comma seperator)
+        output message to DOM container a HTML element body
+        fancy output including amount taxed per range
+    5) Prove your solution in at least two browsers
+        Test income for each range
+*/
+
+function displayError(message) {
+  utils.print(message);
+}
+
+function clearError() {}
+
+function displayTaxAmount(message) {
+  utils.print(message); // eslint-disable-line no-console
+}
+
+// Step 1 & 2
+$('#income').change(() => {
+  const amount = null;
+  if (isNaN(amount)) { // letter is found (not digit)
+    displayError('Not a number');
+    return; // break out when invalid
+  }
+  clearError();
+
+  // Step 4
+  let taxAmount = calculateTax(amount);
+  taxAmount = currencyFormat(taxAmount);
+  displayTaxAmount(taxAmount);
+});
