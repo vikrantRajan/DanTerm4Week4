01232e18f204ceb751f67e1b9e9eadef5f607657
VanArts <instructor@vanarts.com>
2016-10-19 11:18:01 -0700

    Solution: Actor dropdown menu

--- src/js/class07/readFilm.js
+++ src/js/class07/readFilm.js
@@ -5,7 +5,9 @@ function readFilm() {
   // *What* may need a loop??
 
   // jQuery pattern is selector -> method (what)
-  $('body').append(film.movies[0].abridged_cast[2].name);
+  $.each(film.movies[0].abridged_cast, (x, actor) => {
+    $('#actors').append(`<option>${actor.name}</option>`);
+  });
 }
 
 // If Node.js then export as public
