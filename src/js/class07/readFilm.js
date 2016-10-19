45f7c966b4d41ce58d9ced3b2c5b4f9586d1eb0b
VanArts <instructor@vanarts.com>
2016-10-19 11:00:18 -0700

    Exercise: Display a dropdown of actors

--- src/js/class07/readFilm.js
+++ src/js/class07/readFilm.js
@@ -1,5 +1,10 @@
 /* global film */
 function readFilm() {
+  // display where? DOM -> selector body
+  // display what? Harrison Ford > film.movies[0].abridged_cast[2]
+  // *What* may need a loop??
+
+  // jQuery pattern is selector -> method (what)
   $('body').append(film.movies[0].abridged_cast[2].name);
 }
 
