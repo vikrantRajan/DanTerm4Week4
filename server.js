8d92a8a148f682029e8a95bce72e11cdf0e1ec88
VanArts <instructor@vanarts.com>
2016-10-26 10:10:40 -0700

    Exercise: AJAX connect to local server

--- server.js
+++ server.js
@@ -72,6 +72,36 @@ server.route({
   config: { tags: ['starter'] },
 });
 
+/*
+ *        #    ######  ###
+ *       # #   #     #  #
+ *      #   #  #     #  #
+ *     #     # ######   #
+ *     ####### #        #
+ *     #     # #        #
+ *     #     # #       ###
+ *
+ */
+server.route({
+  method: 'GET',
+  path: '/api/fruit',
+  handler: (request, reply) => {
+    // Browser web address http://localhost:3000/api/fruit?format=json
+    console.log(request.query.format); // eslint-disable-line no-console
+    // PHP is echo($_GET['format']) // outputs blank, xml, json
+    if (request.query.format === 'xml') {
+      const response = reply('<fruits><fruit name="apple">green</fruit><fruit name="banana">yellow</fruit><fruit name="cherry">red</fruit></fruits>');
+      response.type('application/xml');
+    } else { // default of JSON
+      reply({
+        apple: 'green',
+        banana: 'yellow',
+        cherry: 'red',
+      });
+    }
+  },
+});
+
 server.start(() => {
   console.log('Server running at:', server.info.uri); // eslint-disable-line no-console
 });
