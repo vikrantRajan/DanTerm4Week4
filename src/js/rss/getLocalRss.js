aa2fa1956485964a36145c209ad770cf39d7b17e
VanArts <instructor@vanarts.com>
2016-11-02 11:50:01 -0700

    Solution: Display news summary

--- src/js/rss/getLocalRss.js
+++ src/js/rss/getLocalRss.js
@@ -3,9 +3,13 @@ function getLocalRss() {
     url: '/jquery/rss/cbc-technology.xml',
     success: (response) => {
       const $news = $('#news');
+
       $(response).find('item').each((index, item) => {
         const title = $(item).children('title').text();
-        $news.append(`<li>${title}</li>`);
+        const link = $(item).children('link').text();
+        const description = $(item).children('description').text();
+
+        $news.append(`<li><a href="${link}">${title}</a><p>${description}</p></li>`);
       });
     },
   });
