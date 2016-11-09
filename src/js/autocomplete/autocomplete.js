ee6f25a317edbfa831cd318d1af411ef1d04ced6
VanArts <instructor@vanarts.com>
2016-11-09 12:16:52 -0800

    Advanced nice to know: Arrow keys highlight

--- src/js/autocomplete/autocomplete.js
+++ src/js/autocomplete/autocomplete.js
@@ -1,3 +1,42 @@
+/* global document */
+const keys = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+};
+let autocompletePosition = 0;
+function hightlightSuggestion(direction) {
+  if (direction === 'down') {
+    autocompletePosition += 1;
+  } else if (direction === 'up') {
+    autocompletePosition -= 1;
+  }
+
+  $('#country_suggestions .hightlight').removeClass('hightlight');
+  const hightlightValue = $(`#country_suggestions li:nth-child(${autocompletePosition})`)
+    .addClass('hightlight')
+    .text();
+
+  $('#country_keywords').val(hightlightValue);
+}
+function listenForArrows() {
+  $(document).keydown((event) => {
+    switch (event.which) {
+      case keys.up:
+        hightlightSuggestion('up');
+        break;
+
+      case keys.down:
+        hightlightSuggestion('down');
+        break;
+
+      default: return; // exit this handler for other keys
+    }
+
+    event.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+}
 function toggleSpinner() {
   $('#spinner').toggleClass('hide');
 }
@@ -22,6 +61,7 @@ function callService(keyword) {
       keyword,
     },
     success: (response) => {
+      listenForArrows();
       formatCountries(response.items);
     },
   });
@@ -29,6 +69,10 @@ function callService(keyword) {
 
 function autocomplete() {
   $('#country_keywords').keyup((event) => {
+    if (event.which === keys.up || event.which === keys.down) {
+      return;
+    }
+
     toggleSpinner();
     callService($(event.target).val());
   });
