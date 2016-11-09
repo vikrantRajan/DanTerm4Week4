9de82c1ef5a46c01a32957bfeebac0ce9fa41b08
VanArts <instructor@vanarts.com>
2016-11-09 09:29:56 -0800

    Solution: Change RSS Providers

---
+++ src/js/rss/changeRssProviders.js
@@ -0,0 +1,29 @@
+function changeRssProvider() {
+  $('#providers').change(() => {
+    const address = $('#providers').val();
+    const $news = $('#news').text('');
+    if (address === '') {
+      return;
+    }
+
+    $.ajax({
+      url: `/api/rss?url=${address}`,
+      success: (response) => {
+        $(response).find('item').each((index, item) => {
+          const title = $(item).children('title').text();
+          const link = $(item).children('link').text();
+          const description = $(item).children('description').text();
+
+          $news.append(`<li><a href="${link}">${title}</a><p>${description}</p></li>`);
+        });
+      },
+    });
+  });
+}
+
+// If Node.js then export as public
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    changeRssProvider,
+  };
+}
