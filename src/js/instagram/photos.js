fead6cd2c72cfbb6e088023884b240f957012924
VanArts <instructor@vanarts.com>
2017-02-01 10:23:57 -0800

    Instagram solution: Login link then reload for thumbs

--- src/js/instagram/photos.js
+++ src/js/instagram/photos.js
@@ -2,7 +2,11 @@ function instagram() {
   $.ajax({
     url: '/api/instagram',
     success: (response) => {
-      console.log(response);
+      const html = [];
+      response.forEach((photo) => {
+        html.push(`<img src="${photo.images.thumbnail.url}" width="${photo.images.thumbnail.width}" height="${photo.images.thumbnail.height}">`);
+      });
+      $('body').append(html.join(''));
     },
   });
 }
