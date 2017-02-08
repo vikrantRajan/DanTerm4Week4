24e8f067ff734440adad51b771ac59ad77592eb1
VanArts <instructor@vanarts.com>
2017-02-08 11:51:33 -0800

    QR Code: Solution with QR image

--- src/js/qrCode/mecard.js
+++ src/js/qrCode/mecard.js
@@ -8,6 +8,14 @@ function mecardFormat(data) {
   return mecard.join('');
 }
 
+function textareaToQr() {
+  const apiEndpoint = 'https://chart.googleapis.com/chart';
+  const apiArgs = '?cht=qr&chs=300x300&chl=';
+  const mecard = $('#mecard').val();
+
+  $('#qr_code').attr('src', apiEndpoint + apiArgs + mecard);
+}
+
 function buildMecard() {
   $('form').on('submit', (event) => {
     const fname = $('#fname').val();
@@ -25,6 +33,8 @@ function buildMecard() {
 
     event.preventDefault(); // cancel form action
   });
+
+  $('#showQr').click(textareaToQr);
 }
 
 // If Node.js then export as public
