fae81ee39c9422dff1e9674183750c7b8b9802b7
VanArts <instructor@vanarts.com>
2017-02-15 10:46:49 -0800

    Slippy Map: Bubble pin

--- src/js/slippyMap/apiSchool.js
+++ src/js/slippyMap/apiSchool.js
@@ -1,22 +1,22 @@
 /* global document, google */
 
-let map;
-
 function createMap(option) {
-  map = new google.maps.Map(document.getElementById('map'), {
+  return new google.maps.Map(document.getElementById('map'), {
     zoom: 19, // 1 is earth view from space, 20 is sidewalk zoom
     center: option.center,
   });
 }
 
 function createPin(options) {
+  const map = options.map;
+
   const marker = new google.maps.Marker({
     position: options.geocode,
     map,
   });
 
   const infowindow = new google.maps.InfoWindow({
-    content: options.html,
+    content: options.htmlBubble,
   });
 
   marker.addListener('click', () => {
@@ -27,8 +27,8 @@ function createPin(options) {
 function initMap() {
   const vanarts = { lat: 49.28270005993911, lng: -123.11536252498627 };
 
-  createMap({ center: vanarts });
-  createPin({ geocode: vanarts, html: '<b>Hello</b> World' });
+  const map = createMap({ center: vanarts });
+  createPin({ geocode: vanarts, htmlBubble: '<b>Hello</b> World', map });
 }
 
 // If Node.js then export as public
