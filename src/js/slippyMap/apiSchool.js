3d0354ab1bb9a8436cf1bd54def60fd57bca56d6
VanArts <instructor@vanarts.com>
2017-02-15 10:22:12 -0800

    Slippy Map: Solution school pin

--- src/js/slippyMap/apiSchool.js
+++ src/js/slippyMap/apiSchool.js
@@ -1,15 +1,15 @@
 /* global document, google */
 
 function initMap() {
-  const myLatLng = { lat: -25.363, lng: 131.044 };
+  const vanarts = { lat: 49.28270005993911, lng: -123.11536252498627 };
 
   const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatLng,
+    zoom: 19, // 1 is earth view from space, 20 is sidewalk zoom
+    center: vanarts,
   });
 
   const marker = new google.maps.Marker({
-    position: myLatLng,
+    position: vanarts,
     map,
     title: 'Hello VanArts',
   });
