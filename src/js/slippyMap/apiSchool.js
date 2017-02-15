56976a9bc5b569b19ec963c59bf5cfda2886d873
VanArts <instructor@vanarts.com>
2017-02-15 10:36:48 -0800

    Slippy Map: Solution info bubble

--- src/js/slippyMap/apiSchool.js
+++ src/js/slippyMap/apiSchool.js
@@ -1,20 +1,34 @@
 /* global document, google */
 
+let map;
+
+function createMap(option) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 19, // 1 is earth view from space, 20 is sidewalk zoom
+    center: option.center,
+  });
+}
+
+function createPin(options) {
+  const marker = new google.maps.Marker({
+    position: options.geocode,
+    map,
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: options.html,
+  });
+
+  marker.addListener('click', () => {
+    infowindow.open(map, marker);
+  });
+}
+
 function initMap() {
   const vanarts = { lat: 49.28270005993911, lng: -123.11536252498627 };
 
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 19, // 1 is earth view from space, 20 is sidewalk zoom
-    center: vanarts,
-  });
-
-  const marker = new google.maps.Marker({
-    position: vanarts,
-    map,
-    title: 'Hello VanArts',
-  });
-
-  console.log(marker);
+  createMap({ center: vanarts });
+  createPin({ geocode: vanarts, html: '<b>Hello</b> World' });
 }
 
 // If Node.js then export as public
