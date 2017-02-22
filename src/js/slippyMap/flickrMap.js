886f69f271faa97b009e9fb323b8056ef3ff5b6f
VanArts <instructor@vanarts.com>
2017-02-22 10:01:33 -0800

    Slippy Map: Solution center and zoom based on pins

--- src/js/slippyMap/flickrMap.js
+++ src/js/slippyMap/flickrMap.js
@@ -1,17 +1,20 @@
 /* global document, google */
 
 function createMap(options) {
+  const center = (options && options.center) ? { lat: options.center.latitude, lng: options.center.longitude } : null;
+
   return new google.maps.Map(document.getElementById('map'), {
     zoom: 19, // 1 is earth view from space, 20 is sidewalk zoom
-    center: {
-      lat: options.center.latitude,
-      lng: options.center.longitude,
-    },
+    center,
   });
 }
 
 function createPin(options) {
   const map = options.map;
+
+  if (options.pinBounds) {
+    options.pinBounds.extend(new google.maps.LatLng(options.geocode.latitude, options.geocode.longitude));
+  }
 
   const marker = new google.maps.Marker({
     position: {
@@ -34,12 +37,14 @@ function initFlickrMap() {
   $.ajax({
     url: '/api/flickr/geo',
     success: (response) => {
-      const firstPhoto = response.items[0];
-      const map = createMap({ center: firstPhoto });
+      const pinBounds = new google.maps.LatLngBounds();
+      const map = createMap();
 
       response.items.forEach((item) => {
-        createPin({ geocode: item, htmlBubble: '<b>Hello</b> World', map });
+        createPin({ geocode: item, htmlBubble: '<b>Hello</b> World', map, pinBounds });
       });
+
+      map.fitBounds(pinBounds);
     },
   });
 }
