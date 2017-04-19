c81ec7b956487bbddadbdcb3ebd7d0050edad191
Dan BROOKS <dan@vanarts.com>
2017-04-19 10:30:07 -0700

    Sales Tax: Step 3 solution

--- src/js/tax/sales.js
+++ src/js/tax/sales.js
@@ -1,10 +1,17 @@
 /* global salesTaxData */
+function provincesChanged() {
+  const $dropdown = $(this);
+  console.log($dropdown.val());
+}
 
 function sales() {
+  const $provinces = $('#provinces');
   // Object
   $.each(salesTaxData.provinces, (provinceAbbr, provinceData) => { // propertyName, valueOfProperty
-    $('#provinces').append(`<option value="${provinceData.taxes[0].tax}">${provinceData.name}</option>`);
+    $provinces.append(`<option value="${provinceData.taxes[0].tax}">${provinceData.name}</option>`);
   });
+
+  $provinces.change(provincesChanged);
 }
 
 // If Node.js then export as public
