b617e09213e932152fc8433d1a473860ffc9a736
Dan BROOKS <dan@vanarts.com>
2017-04-19 09:01:48 -0700

    Slideshow: JSON

--- src/slides.js
+++ src/slides.js
@@ -1,12 +1,12 @@
 exports.register = (server, pluginOptions, next) => {
+  const slides = ['client-side', 'js-var-types', 'cdn', 'js-events', 'json'];
+
   server.route({
     method: 'GET',
     path: '/',
-    handler: (request, reply) => reply.view('slides'),
+    handler: (request, reply) => reply.view('slides', { slides }),
     config: { tags: ['starter'] }
   });
-
-  const slides = ['client-side', 'js-var-types', 'cdn', 'js-events'];
 
   slides.forEach((slideName) => {
     server.route({
