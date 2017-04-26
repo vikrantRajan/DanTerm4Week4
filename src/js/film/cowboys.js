56a93e80c165879503dda933c5c779d47fd0fe83
Dan BROOKS <dan@vanarts.com>
2017-04-26 10:25:20 -0700

    Cowboys: Dropdown solution

--- src/js/film/cowboys.js
+++ src/js/film/cowboys.js
@@ -9,9 +9,21 @@ function cowboys() {
   $('body').append(film.movies[0].abridged_cast[2].name);
 }
 
+function cowboysDropdown() {
+  // display where? DOM -> selector body
+  // display what? Harrison Ford > film.movies[0].abridged_cast[2]
+  // *What* may need a loop??
+
+  // jQuery pattern is selector -> method (what)
+  $.each(film.movies[0].abridged_cast, (x, actor) => {
+    $('#actors').append(`<option>${actor.name}</option>`);
+  });
+}
+
 // If Node.js then export as public
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = {
-    cowboys
+    cowboys,
+    cowboysDropdown
   };
 }
