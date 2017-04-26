5189fb1a62d27abf4903c019be47f056378172c7
Dan BROOKS <dan@vanarts.com>
2017-04-26 11:18:15 -0700

    New route: Fruit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,24 @@
 exports.register = (server, pluginOptions, next) => {
+  server.route({
+    method: 'GET',
+    path: '/fruit',
+    handler: (request, reply) => {
+      // Browser web address http://localhost:3000/api/fruit?format=json
+      console.log(request.query.format); // output blank, xml, json
+      // PHP is echo($_GET['format']) // outputs blank, xml, json
+      if (request.query.format === 'xml') {
+        const response = reply('<fruits><fruit name="apple">green</fruit><fruit name="banana">yellow</fruit><fruit name="cherry">red</fruit></fruits>');
+        response.type('application/xml');
+      } else { // default of JSON
+        reply({
+          apple: 'green',
+          banana: 'yellow',
+          cherry: 'red'
+        });
+      }
+    }
+  });
+
   next();
 };
 
