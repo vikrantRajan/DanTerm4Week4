bc100d4dc8f1a9ff595336f03f92b3be2e4a8f6b
Dan BROOKS <dan@vanarts.com>
2017-05-03 10:59:24 -0700

    Exercise: XML solution

--- src/js/fruit/xml.js
+++ src/js/fruit/xml.js
@@ -1,10 +1,19 @@
+function displayFruits(response) {
+  const $fruits = $('#fruits');
+
+  $(response).find('fruit').each((index, element) => {
+    const colour = $(element).text();
+    const fruitName = $(element).attr('name');
+
+    $fruits.append(`<li style="background-color: ${colour}">${fruitName}</li>`);
+  });
+}
+
 function fruitXml() {
   // When? Immediately or based on user interaction?
   const options = {
     url: '/api/fruit?format=xml',
-    success: (response) => {
-      console.log(response);
-    }
+    success: displayFruits
   };
   $.ajax(options);
 }
