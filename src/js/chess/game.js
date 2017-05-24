cdf5cd4e1b87402c3bd15be56a3b912d2e52f2c5
Dan BROOKS <dan@vanarts.com>
2017-05-24 11:09:37 -0700

    Chess: Arrow solution

--- src/js/chess/game.js
+++ src/js/chess/game.js
@@ -1,4 +1,40 @@
+/* global document */
+
+function listenForArrows() {
+  const keys = {
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40
+  };
+
+  $(document).keydown((event) => {
+    switch (event.which) {
+      case keys.left:
+        console.log('left');
+        break;
+
+      case keys.up:
+        console.log('up');
+        break;
+
+      case keys.right:
+        console.log('right');
+        break;
+
+      case keys.down:
+        console.log('down');
+        break;
+
+      default: return; // exit this handler for other keys
+    }
+
+    event.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+}
+
 function playChess() {
+  listenForArrows();
   $('.piece').click((element) => {
     // find any/all "highlight" CSS classes then remove .highlight
     $('.highlight').removeClass('highlight');
