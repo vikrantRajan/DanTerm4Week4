29b53255cbeda7563eb06bd86886d06018d75be7
Dan BROOKS <dan@vanarts.com>
2017-05-24 12:17:20 -0700

    Chess: Move within boundary solution

--- src/js/chess/game.js
+++ src/js/chess/game.js
@@ -7,21 +7,26 @@ function listenForArrows() {
     right: 39,
     down: 40
   };
+  const boardCorners = {
+    leftTop: 50,
+    rightBottom: 750
+  };
 
   $(document).keydown((event) => {
     let isArrow = false;
     const $chessPiece = $('.highlight');
+    const currentPosition = $chessPiece.position();
 
-    if (event.which === keys.left) {
+    if (event.which === keys.left && currentPosition.left > boardCorners.leftTop) {
       $chessPiece.css('left', '-=100');
       isArrow = true;
-    } else if (event.which === keys.up) {
+    } else if (event.which === keys.up && currentPosition.top > boardCorners.leftTop) {
       $chessPiece.css('top', '-=100');
       isArrow = true;
-    } else if (event.which === keys.right) {
+    } else if (event.which === keys.right && currentPosition.left < boardCorners.rightBottom) {
       $chessPiece.css('left', '+=100');
       isArrow = true;
-    } else if (event.which === keys.down) {
+    } else if (event.which === keys.down && currentPosition.top < boardCorners.rightBottom) {
       $chessPiece.css('top', '+=100');
       isArrow = true;
     }
