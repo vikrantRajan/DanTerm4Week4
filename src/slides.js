56cf55c191ec675cc3f368a0742044df76b6f5b5
Dan BROOKS <dan@vanarts.com>
2017-08-05 12:51:30 -0700

    Add Slide: Twitter

--- src/slides.js
+++ src/slides.js
@@ -1,5 +1,5 @@
 exports.register = (server, pluginOptions, next) => {
-  const slides = ['client-side', 'js-var-types', 'cdn', 'js-events', 'json', 'ajax', 'git', 'api', 'qrcode', 'flickr'];
+  const slides = ['client-side', 'js-var-types', 'cdn', 'js-events', 'json', 'ajax', 'git', 'api', 'qrcode', 'flickr', 'twitter'];
 
   server.route({
     method: 'GET',
