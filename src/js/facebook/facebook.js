6d0c79d1093f344cfde7985a7351f0daacab056f
Dan BROOKS <dan@vanarts.com>
2017-08-23 11:56:26 -0700

    Facebook Solution: Profile picture

--- src/js/facebook/facebook.js
+++ src/js/facebook/facebook.js
@@ -7,9 +7,19 @@ function facebookPage() {
   });
 }
 
+function facebookProfile() {
+  $.ajax({
+    url: '/api/facebook?object=567323233?fields=picture',
+    success: (response) => {
+      $('body').append(`<img src="${response.picture.data.url}">`);
+    }
+  });
+}
+
 // If Node.js then export as public
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = {
-    facebookPage
+    facebookPage,
+    facebookProfile
   };
 }
