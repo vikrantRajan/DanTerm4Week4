d3c02837a82c3815da9ddc620495d02420d9750d
danactive <dan@domaindesign.ca>
2017-09-18 09:34:57 -0700

    Calendar: Reschedule Wednesday Oct 4th class to Nov 22

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -63,7 +63,8 @@ function createCourseSchedule(courseTitle = 'Dan\'s class') {
 function courseCalendar() {
   const firstDate = new Date(2017, 8, 6); // Sept 6th
   const course = createCourseSchedule('jQuery')
-    .setFirstClassDate(firstDate);
+    .setFirstClassDate(firstDate)
+    .setDaysBetweenClasses([7, 7, 7, 14, 7, 7, 7, 7, 7, 7, 7]);
   const events = course.getAllClasses();
 
   $('#calendar').fullCalendar({
