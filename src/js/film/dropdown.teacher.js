b7157156d676083c7f023a052bf0076ee9fad9a2
danactive <dan@vanarts.com>
2017-10-25 11:35:04 -0700

    Film - Solution dev step 3

--- src/js/film/dropdown.teacher.js
+++ src/js/film/dropdown.teacher.js
@@ -1,10 +1,15 @@
 /* global utils */
 
+function populateActors(response) {
+  const harrison = response.movies[0].abridged_cast[2].name;
+  $('#actors').append(`<option>${harrison}</option>`);
+}
+
 function getFilmData() {
   $.ajax({
     url: 'cowboy.json',
     success: (response) => {
-      utils.print(response);
+      populateActors(response);
     }
   });
 }
