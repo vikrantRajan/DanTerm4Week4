8c4368f716921c584d3878fd13aadecc3fdc642d
danactive <dan@vanarts.com>
2017-11-01 11:04:34 -0700

    Fruit - Coloured fruit list

--- src/js/fruit/spinner.teacher.js
+++ src/js/fruit/spinner.teacher.js
@@ -1,11 +1,19 @@
-function displayFruit(response) {
-  console.log(response);
+function appendFruit(fruitName, colour) {
+  $('#fruits')
+    .append(`<li style="background-color: ${colour}">${fruitName}</li>`);
+}
+
+function parseResponse(response) {
+  Object.keys(response).forEach((fruitName) => {
+    appendFruit(fruitName, response[fruitName]);
+  });
 }
 
 function fruitSpinner() {
+  $('#fruits').empty(); // delete the HTML <li>Todo</li>
   $.ajax({
     url: '/api/slow-fruit',
-    success: displayFruit
+    success: parseResponse
   });
 }
 
