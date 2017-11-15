57d4305e14d61b5c4abdc09b70e03fd3fab52892
danactive <dan@vanarts.com>
2017-11-15 12:28:57 -0800

    Autocomplete - Solution #3

--- src/js/autocomplete/input.teacher.js
+++ src/js/autocomplete/input.teacher.js
@@ -1,10 +1,34 @@
+function displayCountries(output) {
+  $('#country_suggestions').html(output);
+}
+
+function formatCountries(countries) {
+  // Imperative programming
+  // The term is often used in contrast to declarative programming, which focuses on what the program should accomplish without specifying how the program should achieve the result.
+
+  // const html = [];
+  // $.each(countries, (index, country) => {
+  //   html.push(`<li>${country}</li>`);
+  // });
+
+  // ES6 map single line
+  // declarative paradigm - specically functional programming is ideal
+  const html = countries.map(country => `<li>${country}</li>`);
+
+  // ES6 map multiline
+  // const html = countries.map((country) => {
+  //   return `<li>${country}</li>`;
+  // });
+
+  displayCountries(html.join(''));
+}
+
 function queryService(keyword) {
   $.ajax({
     url: '/api/autocomplete',
     data: { keyword },
     success: (response) => {
-      // todo format in own function
-      $('#country_suggestions').html(`<li>${response.items}</li>`);
+      formatCountries(response.items);
     }
   });
 }
