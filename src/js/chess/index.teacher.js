b7f9ae81ddbda011e96d47b85add70f9bbff59e8
danactive <dan@vanarts.com>
2017-11-22 11:09:14 -0800

    Chess - Solution #3

--- src/js/chess/index.teacher.js
+++ src/js/chess/index.teacher.js
@@ -1,3 +1,29 @@
+function listenForChessArrows() {
+  $(document).keydown((event) => {
+    switch (event.which) {
+      case keys.up:
+        console.log('up');
+        break;
+
+      case keys.right:
+        console.log('right');
+        break;
+
+      case keys.down:
+        console.log('down');
+        break;
+
+      case keys.left:
+        console.log('left');
+        break;
+
+      default: return; // exit this handler for other keys
+    }
+
+    event.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+}
+
 function highlightPiece() {
   $('.highlight').removeClass('highlight');
   $(this).addClass('highlight');
@@ -9,6 +35,7 @@ function bindDom() {
 
 function playChess() {
   bindDom();
+  listenForChessArrows();
 }
 
 // If Node.js then export as public
