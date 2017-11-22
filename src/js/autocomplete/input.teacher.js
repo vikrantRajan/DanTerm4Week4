764c5ace1f8964683890ca885e17fae3832c0fa7
danactive <dan@vanarts.com>
2017-11-22 10:21:40 -0800

    Autocomplete - Homework solution

--- src/js/autocomplete/input.teacher.js
+++ src/js/autocomplete/input.teacher.js
@@ -1,31 +1,70 @@
+/* global document */
+
+let autocompletePosition = 0;
+const keys = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40
+};
+
+function hightlightSuggestion(direction) {
+  if (direction === 'down') {
+    autocompletePosition += 1;
+  } else if (direction === 'up') {
+    autocompletePosition -= 1;
+  }
+
+  $('#country_suggestions .hightlight').removeClass('hightlight');
+  const hightlightValue = $(`#country_suggestions li:nth-child(${autocompletePosition})`)
+    .addClass('hightlight')
+    .text();
+
+  $('#country_keywords').val(hightlightValue);
+}
+
+function listenForArrows() {
+  $(document).keydown((event) => {
+    switch (event.which) {
+      case keys.up:
+        hightlightSuggestion('up');
+        break;
+
+      case keys.down:
+        hightlightSuggestion('down');
+        break;
+
+      default: return; // exit this handler for other keys
+    }
+
+    event.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+}
+
+function toggleSpinner() {
+  $('#spinner').toggleClass('hide');
+}
+
 function displayCountries(output) {
-  $('#country_suggestions').html(output);
+  toggleSpinner();
+  $('#country_suggestions').html(output).addClass('expanded');
 }
 
 function formatCountries(countries) {
-  // Imperative programming
-  // The term is often used in contrast to declarative programming
-  // which focuses on what the program should accomplish without
-  // specifying how the program should achieve the result.
-
   // const html = [];
   // $.each(countries, (index, country) => {
   //   html.push(`<li>${country}</li>`);
   // });
 
-  // ES6 map single line
-  // declarative paradigm - specically functional programming is ideal
+  // ES6 advanced code
   const html = countries.map(country => `<li>${country}</li>`);
-
-  // ES6 map multiline
-  // const html = countries.map((country) => {
-  //   return `<li>${country}</li>`;
-  // });
 
   displayCountries(html.join(''));
 }
 
 function queryService(keyword) {
+  toggleSpinner();
+  autocompletePosition = 0; // reset the keyboard position
   $.ajax({
     url: '/api/autocomplete',
     data: { keyword },
@@ -37,13 +76,18 @@ function queryService(keyword) {
 
 function listenToInput() {
   $('#country_keywords').keyup((event) => {
+    if (event.which === keys.up || event.which === keys.down) {
+      return;
+    }
+
     const keyword = $(event.target).val();
     queryService(keyword);
   });
 }
 
-function autocomplete() { // HTML initates
+function autocomplete() {
   listenToInput();
+  listenForArrows();
 }
 
 // If Node.js then export as public
