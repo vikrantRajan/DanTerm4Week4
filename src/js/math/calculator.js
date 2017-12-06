ced4217bc3f927629689b46dc0f86712da2b7d29
danactive <dan@vanarts.com>
2017-12-06 11:16:54 -0800

    Calculator solution

---
+++ src/js/math/calculator.js
@@ -0,0 +1,34 @@
+/* global document, difference, sum */
+function bindCalc() {
+  const numA = document.getElementById('numA');
+  const numB = document.getElementById('numB');
+  const operator = document.getElementById('operator');
+
+  const calc = () => {
+    const math = (operator.value === 'Add') ? sum : difference;
+    log(math(numA.value, numB.value));
+  };
+  const log = (message) => console.log(message);
+
+  numA.addEventListener('change', calc);
+  numB.addEventListener('change', calc);
+  operator.addEventListener('change', calc);
+
+
+
+  $('#numA, #numB, #operator').change(() => {
+    const numA = $('#numA').val();
+    const numB = $('#numB').val();
+    const operator = $('#operator').val();
+
+    const math = (operator === 'Add') ? sum : difference;
+    log(math(numA, numB));
+  });
+}
+
+// If Node.js then export as public
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    calc
+  };
+}
