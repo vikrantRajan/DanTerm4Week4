458785ef2fe71031796b25a3c77ed6f565f098e7
danactive <dan@vanarts.com>
2017-12-13 11:32:25 -0800

    vCard solution

--- src/js/qrcode/generate.js
+++ src/js/qrcode/generate.js
@@ -10,9 +10,40 @@ function createQrBlob() {
   });
 }
 
+function buildVcard() {
+  $('form').on('submit', (event) => {
+    const fname = $('#fname').val();
+    const lname = $('#lname').val();
+    const title = $('#title').val();
+    const url = $('#url').val();
+    const vcard = ['BEGIN:VCARD\nVERSION:3.0'];
+
+    vcard.push('N:', lname, ';', fname, '\n');
+    vcard.push('FN:', fname, ' ', lname, '\n');
+    vcard.push('TITLE:', title, '\n');
+    vcard.push('URL:', url, '\n');
+    vcard.push('END:VCARD'); // close vcard
+
+    $('#vcard').val(vcard.join(''));
+
+//     const vcardTemplate = `BEGIN:VCARD
+// VERSION:3.0
+// N: ${lname}; ${fname}
+// FN ${fname} ${lname}
+// TITLE: ${title}
+// URL: ${url}
+// END:VCARD`;
+
+//     $('#vcard').val(vcardTemplate);
+
+    event.preventDefault(); // cancel form action
+  });
+}
+
 // If Node.js then export as public
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = {
-    createQrBlob
+    createQrBlob,
+    buildVcard
   };
 }
