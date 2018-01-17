7345e550dd799b9caf1e0b7eea3e79420a486913
danactive <dan@vanarts.com>
2018-01-17 10:09:04 -0800

    Twitter: HTML displays tweets

--- src/js/twitter/tweets.js
+++ src/js/twitter/tweets.js
@@ -1,5 +1,16 @@
 function getTwitterTweets() {
   console.log('Hello Twitter');
+
+  $.ajax({
+    url: '/api/twitter',
+    success: (response) => {
+      const html = [];
+      response.tweets.forEach((item) => {
+        html.push(`<li>${item.text}</li>`);
+      });
+      $('body').append(`<ul>${html.join('')}</ul>`);
+    }
+  });
 }
 
 // If Node.js then export as public
