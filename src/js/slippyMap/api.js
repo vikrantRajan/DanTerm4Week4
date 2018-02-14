1a4c040eab0fcf1ad75bae820fbc6be5466229d8
danactive <dan@vanarts.com>
2018-02-14 11:15:20 -0800

    Slippy Map - Refactor basic map

--- src/js/slippyMap/api.js
+++ src/js/slippyMap/api.js
@@ -1,19 +1,22 @@
 /* global document, google */
-
 function initMap() {
-  const varArtsGeo = { lat: 49.282873, lng: -123.115368 };
-  const options = {
-    center: varArtsGeo,
-    zoom: 20
+  const createMap = (options) => {
+    return new google.maps.Map(document.getElementById('map'), options);
   };
 
-  const map = new google.maps.Map(document.getElementById('map'), options);
+  const createMarker = (location) => {
+    return new google.maps.Marker({ // eslint-disable-line no-new
+      position: location,
+      map,
+      title: location.title
+    });
+  }
 
-  new google.maps.Marker({ // eslint-disable-line no-new
-    position: varArtsGeo,
-    map,
-    title: 'VanArts'
-  });
+  const varArts = { lat: 49.282873, lng: -123.115368, title: 'VanArts' };
+
+  const map = createMap({ center: varArts, zoom: 20 });
+
+  createMarker(varArts);
 }
 
 // If Node.js then export as public
