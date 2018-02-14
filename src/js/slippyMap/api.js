7131c2181cf6a41c34273d45f277fb6ea052b1e7
danactive <dan@vanarts.com>
2018-02-14 11:22:52 -0800

    Slippy Map - Lint

--- src/js/slippyMap/api.js
+++ src/js/slippyMap/api.js
@@ -1,20 +1,16 @@
 /* global document, google */
 function initMap() {
-  const createMap = (options) => {
-    return new google.maps.Map(document.getElementById('map'), options);
-  };
-
-  const createMarker = (location) => {
-    return new google.maps.Marker({ // eslint-disable-line no-new
-      position: location,
-      map,
-      title: location.title
-    });
-  }
+  const createMap = options => new google.maps.Map(document.getElementById('map'), options);
 
   const varArts = { lat: 49.282873, lng: -123.115368, title: 'VanArts' };
 
   const map = createMap({ center: varArts, zoom: 20 });
+
+  const createMarker = location => new google.maps.Marker({
+    position: location,
+    map,
+    title: location.title
+  });
 
   createMarker(varArts);
 }
