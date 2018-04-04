42786670f0000eac86e9690e7a43486b654e1dad
danactive <dan@vanarts.com>
2018-04-04 10:08:40 -0700

    Class 04 - jQuery tree solution

---
+++ src/js/basic-interaction/events.js
@@ -0,0 +1,21 @@
+// function Hello() {
+// 	const css = { color: 'green' };
+
+//   function logHello() {
+//   	console.log('Hello from React.js');
+//   }
+
+//   return (
+//   	<div
+//       onClick={logHello}
+//       style={css}
+//     >
+//       Hello from React.js
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+// 	<Hello />,
+//   document.getElementById('container')
+// );
