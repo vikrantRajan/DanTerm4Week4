429e2f679702dc4ba49042a1b2682c58e3cfa1d8
danactive <dan@vanarts.com>
2018-04-04 11:23:54 -0700

    Class 04 - Image rollover console log

---
+++ src/js/image-rollover/stateless.js
@@ -0,0 +1,22 @@
+// function Hello() {
+// 	const css = {};
+
+//   function logHello(event) {
+//   	const path = event.target.getAttribute('data-swap');
+//   	event.target.src = path;
+//   }
+
+//   return (
+//   	<img
+//       src="http://cache1.asset-cache.net/xt/146788260.jpg?v=1&g=fs1|0|SKP102|88|260&s=1"
+//       data-swap="http://cache4.asset-cache.net/xt/122556288.jpg?v=1&g=fs1|0|ING|56|288&s=1"
+//       onMouseOver={logHello}
+//       style={css}
+//     />
+//   );
+// }
+
+// ReactDOM.render(
+// 	<Hello />,
+//   document.getElementById('container')
+// );
