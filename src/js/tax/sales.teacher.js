0d3dda1c571ad64b9f4c75b2c054d0a0ac308673
danactive <dan@vanarts.com>
2018-04-18 11:27:26 -0700

    Tax step 2 solution

--- src/js/tax/sales.teacher.js
+++ src/js/tax/sales.teacher.js
@@ -1,63 +1,28 @@
-/* global utils */
+/* global document, salesTaxData */
+// function displayProvinces(salesTaxData) {
+//   const provincesDropdown = $('#provinces');
 
-function outputTotalPrice(price) {
-  const priceGrouped = price.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-  const currency = `$${priceGrouped}`;
-
-  $('#pizzaCost').text(currency);
-}
-
-function calculateTotalPrice(salesValue, pizzaValue) {
-  const salesTax = 1 + Number(salesValue); // preferred over parseInt(salesValue, 10)
-  const pizzaPrice = Number(pizzaValue);
-  outputTotalPrice(salesTax * pizzaPrice);
-}
-
-function updatePrice() {
-  calculateTotalPrice($('#provinces').val(), $('#price').val());
-}
-
-function calculateTax(data) {
-  let taxTotal = data.taxes[0].tax;
-  if (data.taxes.length === 2) {
-    taxTotal += data.taxes[1].tax;
-  }
-
-  return taxTotal.toFixed(2); // fix decical to cents
-}
+//   $.each(salesTaxData.provinces, (provinceAbbr, provinceData) => {
+//     provincesDropdown.append(`<option>${provinceData.name}</option>`);
+//   });
+// }
 
 function displayProvinces(salesTaxData) {
-  // jQuery each loop
-  const $provinces = $('#provinces');
+  const provincesDropdown = document.querySelector('#provinces');
 
-  $.each(salesTaxData.provinces, (provinceAbbr, provinceData) => {
-    const tax = calculateTax(provinceData); // todo dev step #3
-    $provinces.append(`<option value="${tax}">${provinceData.name}</option>`);
-  });
+  const html = Object.values(salesTaxData.provinces)
+    .map(item => `<option>${item.name}</option>`);
+
+  $(provincesDropdown).append(html);
 }
 
-function getSalesData() {
-  // request JSON for sales data (provinces and tax)
-  $.ajax({
-    url: 'rates.json',
-    success: (response) => {
-      displayProvinces(response);
-    },
-    error: (jqXHR, textStatus, errorThrown) => {
-      utils.print(errorThrown);
-    }
-  });
-}
-
-function sales() {
-  getSalesData();
-  $('#price').change(updatePrice);
-  $('#provinces').change(updatePrice);
+function pizzaSales() {
+  displayProvinces(salesTaxData);
 }
 
 // If Node.js then export as public
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = {
-    sales
+    pizzaSales,
   };
 }
