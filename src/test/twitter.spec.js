c19abef3d500c7b307b933a8743da22ba539939a
danactive <dan@vanarts.com>
2018-07-04 12:32:44 -0700

    Twitter - White space for unit test

--- src/test/twitter.spec.js
+++ src/test/twitter.spec.js
@@ -3,7 +3,7 @@ const expect = require('expect.js');
 
 const response = require('./fixtures/user_timeline.json'); // Twitter service cached response
 const { twitterTweets } = require('../js/twitter/filter');
-const source = require('../js/twitter/date.js');
+const { formatTwitterDate } = require('../js/twitter/date.js');
 
 describe('Twitter', () => {
   it('should be full length', () => {
@@ -27,37 +27,49 @@ describe('Twitter', () => {
     const timeZoneOffset = feb1.toString().match(/([-+][0-9]+)\s/)[1];
 
     it('calculates up to weeks', () => {
-      expect(source.formatTwitterDate(`Web Feb 01 10:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('just now');
-      expect(source.formatTwitterDate(`Web Feb 01 9:59:00 ${timeZoneOffset} 2017`, feb1)).to.be('1 minute ago');
-      expect(source.formatTwitterDate(`Web Feb 01 9:50:00 ${timeZoneOffset} 2017`, feb1)).to.be('10 minutes ago');
-      expect(source.formatTwitterDate(`Web Feb 01 9:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('1 hour ago');
-      expect(source.formatTwitterDate(`Web Feb 01 0:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('10 hours ago');
-      expect(source.formatTwitterDate(`Tue Jan 31 10:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('Yesterday');
-      expect(source.formatTwitterDate(`Mon Jan 30 10:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('2 days ago');
-      expect(source.formatTwitterDate(`Wed Jan 25 10:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('1 week ago');
-      expect(source.formatTwitterDate(`Wed Jan 18 10:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('2 weeks ago');
-      expect(source.formatTwitterDate(`Wed Jan 04 10:00:00 ${timeZoneOffset} 2017`, feb1)).to.be('4 weeks ago');
+      expect(formatTwitterDate(`Web Feb 01 10:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('just now');
+
+      expect(formatTwitterDate(`Web Feb 01 9:59:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('1 minute ago');
+
+      expect(formatTwitterDate(`Web Feb 01 9:50:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('10 minutes ago');
+
+      expect(formatTwitterDate(`Web Feb 01 9:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('1 hour ago');
+
+      expect(formatTwitterDate(`Web Feb 01 0:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('10 hours ago');
+
+      expect(formatTwitterDate(`Tue Jan 31 10:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('Yesterday');
+
+      expect(formatTwitterDate(`Mon Jan 30 10:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('2 days ago');
+
+      expect(formatTwitterDate(`Wed Jan 25 10:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('1 week ago');
+
+      expect(formatTwitterDate(`Wed Jan 18 10:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('2 weeks ago');
+
+      expect(formatTwitterDate(`Wed Jan 04 10:00:00 ${timeZoneOffset} 2017`, feb1))
+        .to.be('4 weeks ago');
     });
+
     // it('calculates weeks to months', () => {
-    //   expect(source.formatTwitterDate(
-    //     `Wed Dec 07 10:00:00 ${timeZoneOffset} 2016`,
-    //     feb1,
-    //   )).to.be('2 months ago');
+    //   expect(formatTwitterDate(`Wed Dec 07 10:00:00 ${timeZoneOffset} 2016`, feb1))
+    //     .to.be('2 months ago');
 
-    //   expect(source.formatTwitterDate(
-    //     `Wed Oct 05 10:00:00 ${timeZoneOffset} 2016`,
-    //     feb1,
-    //   )).to.be('4 months ago');
+    //   expect(formatTwitterDate(`Wed Oct 05 10:00:00 ${timeZoneOffset} 2016`, feb1))
+    //     .to.be('4 months ago');
 
-    //   expect(source.formatTwitterDate(
-    //     `Wed May 04 10:00:00 ${timeZoneOffset} 2016`,
-    //     feb1,
-    //   )).to.be('10 months ago');
+    //   expect(formatTwitterDate(`Wed May 04 10:00:00 ${timeZoneOffset} 2016`, feb1))
+    //     .to.be('10 months ago');
 
-    //   expect(source.formatTwitterDate(
-    //     `Wed Feb 03 10:00:00 ${timeZoneOffset} 2016`,
-    //     feb1,
-    //   )).to.be('13 months ago');
+    //   expect(formatTwitterDate(`Wed Feb 03 10:00:00 ${timeZoneOffset} 2016`, feb1))
+    //     .to.be('13 months ago');
     // });
   });
 });
