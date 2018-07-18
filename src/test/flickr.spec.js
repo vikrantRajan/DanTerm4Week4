d70aee7a58bc90e47aebec71ae20959c56eaee32
danactive <dan@vanarts.com>
2018-07-18 11:14:13 -0700

    Flickr: Unit test for service

--- src/test/flickr.spec.js
+++ src/test/flickr.spec.js
@@ -1,6 +1,7 @@
 /* global describe, it */
 const expect = require('expect.js');
 
+const response = require('./fixtures/flickr-photos_search.json'); // Flickr service cached response
 const { flickrJpgPath } = require('../api-teacher');
 
 describe('Flickr', () => {
@@ -15,5 +16,19 @@ describe('Flickr', () => {
     const actual = flickrJpgPath(mock)[0];
     const expected = 'https://farm5.staticflickr.com/4493/37665981392_2e105fd543.jpg';
     expect(actual).to.be(expected);
+  });
+
+  describe('Cached service response', () => {
+    it('should be full length', () => {
+      const actual = response.photos.photo.length;
+      const expected = 250;
+      expect(actual).to.be(expected);
+    });
+
+    it('should have the first photo path', () => {
+      const actual = flickrJpgPath(response)[0];
+      const expected = 'https://farm2.staticflickr.com/1781/29620705348_e3a1168604.jpg';
+      expect(actual).to.be(expected);
+    });
   });
 });
