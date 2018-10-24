3a6f9b2a3b028d5f32e580b1516c983d69f7c0b6
Dan BROOKS <dan@vanarts.com>
2018-10-24 11:54:26 -0700

    jQuery solution step #2

--- src/js/tax/jquery.teacher.js
+++ src/js/tax/jquery.teacher.js
@@ -1,16 +1,7 @@
 /* global salesTaxData */
 const pizzaSales = () => {
   $.each(salesTaxData.provinces, (provinceAbbr, provinceObj) => {
-    $('body').append(`Province name is ${provinceObj.name}<br>`);
-  });
-
-  const provinceNames = Object.values(salesTaxData.provinces).map(prov => prov.name);
-  provinceNames.forEach((provinceName) => {
-    console.log(`Province name is ${provinceName}<br>`);
-  });
-
-  Object.keys(salesTaxData.provinces).forEach((key) => {
-    console.log(`Province name is ${salesTaxData.provinces[key].name}<br>`);
+    $('#provinces').append(`<option>${provinceObj.name}</option>`);
   });
 };
 
