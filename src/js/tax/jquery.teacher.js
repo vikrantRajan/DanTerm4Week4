9cceed2b31c7b1afd2e4683223abd53c3c7bd62b
Dan BROOKS <dan@vanarts.com>
2018-10-24 12:11:48 -0700

    Add change event and step 3 sub steps

--- src/js/tax/jquery.teacher.js
+++ src/js/tax/jquery.teacher.js
@@ -1,7 +1,14 @@
 /* global salesTaxData */
 const pizzaSales = () => {
+  const provinceDropdown = $('#provinces');
   $.each(salesTaxData.provinces, (provinceAbbr, provinceObj) => {
-    $('#provinces').append(`<option>${provinceObj.name}</option>`);
+    // todo step 3.1 Add federal sales tax to the option element
+    // todo step 3.2 Update to include both federal and provincial sales taxes
+    provinceDropdown.append(`<option>${provinceObj.name}</option>`);
+  });
+
+  provinceDropdown.change(() => {
+    console.log('province changed', provinceDropdown.val());
   });
 };
 
