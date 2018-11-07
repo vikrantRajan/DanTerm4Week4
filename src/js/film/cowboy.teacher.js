1c3d39059e2f4122900c72016fbefd1dca1b77bc
Dan BROOKS <dan@vanarts.com>
2018-11-07 09:36:08 -0800

    Homework solution: Cowboy

---
+++ src/js/film/cowboy.teacher.js
@@ -0,0 +1,43 @@
+const cowboysDropdown = () => {
+  $.ajax({
+    url: 'cowboy.json',
+    success: (response) => {
+      $.each(response.movies, (indexMovie, movie) => {
+        $.each(movie.abridged_cast, (indexActor, actor) => {
+          $('#actors').append(`<option>${actor.name}</option>`);
+        });
+      });
+    },
+  });
+};
+
+const cowboyHomework = () => {
+  $.ajax({
+    url: '../film/cowboy.json',
+    success: (response) => {
+      $.each(response.movies, (indexMovie, movie) => {
+        $.each(movie.abridged_cast, (indexActor, actor) => {
+          $.each(actor.characters, (indexCharacter, character) => {
+            $('#characters').append(`<option value="${actor.name}">${character}</option>`);
+          });
+        });
+      });
+    },
+    error: (x, xx, errorMessage) => {
+      console.log('AJAX error', errorMessage);
+    },
+  });
+
+  $('#characters').change(() => {
+    const actorName = $('#characters').val();
+    $('#actor').text(actorName);
+  });
+};
+
+// If Node.js then export as public
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    cowboysDropdown,
+    cowboyHomework,
+  };
+}
