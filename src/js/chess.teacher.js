ab2007a473d5d99f8854b9cccd8a3c49a695f53c
Dan BROOKS <dan@vanarts.com>
2018-11-28 11:37:47 -0800

    Chess: Step 3 solution

--- src/js/chess.teacher.js
+++ src/js/chess.teacher.js
@@ -1,4 +1,23 @@
+/* global document */
+
 const playChess = () => {
+  const handleArrow = (event) => {
+    const arrow = {
+      left: 37,
+      up: 38,
+      right: 39,
+      down: 40,
+    };
+
+    // event.which normalized digit representation of a keyboard key (work in IE too)
+    if (event.which === arrow.left
+      || event.which === arrow.up
+      || event.which === arrow.right
+      || event.which === arrow.down) {
+      console.log(event.which);
+    }
+  };
+
   const highlightPiece = function highlightPiece() {
     $('.highlight').removeClass('highlight');
     $(this).addClass('highlight');
@@ -6,6 +25,7 @@ const playChess = () => {
 
   const bindDom = () => {
     $('.piece').click(highlightPiece);
+    $(document).keydown(handleArrow);
   };
 
   bindDom();
