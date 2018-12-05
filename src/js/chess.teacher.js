7dac6ce105a506087dc376a3775d3e14fe19385a
Dan BROOKS <dan@vanarts.com>
2018-12-05 09:47:04 -0800

    Chess: Step 5 solution

--- src/js/chess.teacher.js
+++ src/js/chess.teacher.js
@@ -9,16 +9,27 @@ const playChess = () => {
       down: 40,
     };
 
+    const boardCorners = { // 50, 750 plus decimal
+      leftTop: 51,
+      rightBottom: 749,
+    };
+
     const $chessPiece = $('.highlight');
+    const currentPosition = $chessPiece.position();
+
+    // todo step 5. Only move if allowed. If the future position is within the chess board
+    // todo inclass: only allow left or right arrows if position left is between this range
+    // 50 - 750
+    console.log($chessPiece.position());
 
     // event.which normalized digit representation of a keyboard key (work in IE too)
-    if (event.which === arrow.left) {
+    if (event.which === arrow.left && currentPosition.left > boardCorners.leftTop) {
       $chessPiece.css('left', '-=100px');
-    } else if (event.which === arrow.up) {
+    } else if (event.which === arrow.up && currentPosition.top > boardCorners.leftTop) {
       $chessPiece.css('top', '-=100px');
-    } else if (event.which === arrow.right) {
+    } else if (event.which === arrow.right && currentPosition.left < boardCorners.rightBottom) {
       $chessPiece.css('left', '+=100px');
-    } else if (event.which === arrow.down) {
+    } else if (event.which === arrow.down && currentPosition.top < boardCorners.rightBottom) {
       $chessPiece.css('top', '+=100px');
     }
   };
