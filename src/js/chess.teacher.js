64bd4addc330402721b68889313c220d543b81fc
Dan BROOKS <dan@vanarts.com>
2018-12-05 10:32:43 -0800

    Chess: Final solution

--- src/js/chess.teacher.js
+++ src/js/chess.teacher.js
@@ -17,19 +17,20 @@ const playChess = () => {
     const $chessPiece = $('.highlight');
     const currentPosition = $chessPiece.position();
     let isArrow = false;
+    const options = { easing: 'linear', queue: false };
 
     // event.which normalized digit representation of a keyboard key (work in IE too)
     if (event.which === arrow.left && currentPosition.left > boardCorners.leftTop) {
-      $chessPiece.css('left', '-=100px');
+      $chessPiece.animate({ left: '-=100px' }, options);
       isArrow = true;
     } else if (event.which === arrow.up && currentPosition.top > boardCorners.leftTop) {
-      $chessPiece.css('top', '-=100px');
+      $chessPiece.animate({ top: '-=100px' }, options);
       isArrow = true;
     } else if (event.which === arrow.right && currentPosition.left < boardCorners.rightBottom) {
-      $chessPiece.css('left', '+=100px');
+      $chessPiece.animate({ left: '+=100px' }, options);
       isArrow = true;
     } else if (event.which === arrow.down && currentPosition.top < boardCorners.rightBottom) {
-      $chessPiece.css('top', '+=100px');
+      $chessPiece.animate({ top: '+=100px' }, options);
       isArrow = true;
     }
 
