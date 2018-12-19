6470a0fa0876c31b139d7e3d8e9fcddb7ed53836
Dan BROOKS <dan@vanarts.com>
2018-12-19 10:26:11 -0800

    Flickr web feed solution image only

--- src/js/flickr/webfeed.js
+++ src/js/flickr/webfeed.js
@@ -1,6 +1,23 @@
-/* global displayRssConent */
+/* global document, displayRssConent */
 
 const getFlickrPublicPhotos = () => {
+  const createImg = (src) => {
+    const img = `<li><img src=${src} alt="Gastown, Vancouver"></li>`;
+    document.getElementById('photos').innerHTML += img;
+  };
+
+  $.ajax({
+    url: 'https://api.flickr.com/services/feeds/photos_public.gne?tags=gastown&format=json',
+    jsonpCallback: 'jsonFlickrFeed',
+    dataType: 'jsonp', // JSON with Padding. Whereas padding is the function name that Flickr is wrapping JSON in
+    success: (response) => {
+      response.items.forEach(item => createImg(item.media.m));
+    },
+  });
+};
+
+// not in use, RSS code example
+const displayFlickrRss = () => {
   $.ajax({
     url: '/api/rss',
     data: {
@@ -16,6 +33,7 @@ const getFlickrPublicPhotos = () => {
 // If Node.js then export as public
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = {
+    displayFlickrRss,
     getFlickrPublicPhotos,
   };
 }
