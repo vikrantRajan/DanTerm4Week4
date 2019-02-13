5c22a12310ae1dd46c4bb3c3f3a69de01311346b
Dan BROOKS <dan@vanarts.com>
2019-02-13 11:30:05 -0800

    React - Image Rollover - Reveal secondary

--- public/jQuery/react/imageRollover.jsx
+++ public/jQuery/react/imageRollover.jsx
@@ -1,15 +1,43 @@
 const ImageRollover = (props) => {
   return (
   	<img
-      src="https://ap.rdcpix.com/1851743405/88be4aa1c05cf4c8a1c505d688efb931l-c0xd-w1020_h770_q80.jpg" width={props.width} height={props.height}
+      src="../image-rollover/images/stanley-park-1926.jpg"
+      data-secondary="../image-rollover/images/stanley-park-2004.jpg"
+      width={props.width}
+      height={props.height}
+      onMouseOver={rollOn}
+      onMouseOut={rollOff}
     />
   );
 }
 
+const rollOn = (event) => {
+  const secondaryPath = event.target.getAttribute('data-secondary');
+  event.target.setAttribute('src', secondaryPath);
+};
+const rollOff = () => console.log('off');
+
 const gallery = [
-	<ImageRollover width="100" height="45" />,
-  <ImageRollover width="100" height="55" />,
-  <ImageRollover width="100" height="65" />,
+  <ImageRollover
+    key="img1"
+    width="100"
+    height="45"
+  />,
+  <ImageRollover
+    key="img2"
+    width="100"
+    height="55"
+  />,
+  <ImageRollover
+    key="img3"
+    width="100"
+    height="65"
+  />,
+  <ImageRollover
+    key="img4"
+    width="100"
+    height="75"
+  />,
 ];
 
 ReactDOM.render(
