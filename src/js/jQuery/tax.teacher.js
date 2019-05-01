028e5ef5117567a0c1652c03bc74563ca7f14ab9
danactive <dan@vanarts.com>
2019-05-01 09:59:49 -0700

    Solution Step 4

--- src/js/jQuery/tax.teacher.js
+++ src/js/jQuery/tax.teacher.js
@@ -1,13 +1,24 @@
 /* global salesTaxData */
 
+const calculateTotalTax = (provinceAbbr) => {
+  let totalTax = salesTaxData.provinces[provinceAbbr].taxes[0].tax;
+
+  if (salesTaxData.provinces[provinceAbbr].taxes[1]) {
+    totalTax += salesTaxData.provinces[provinceAbbr].taxes[1].tax;
+  }
+
+  return totalTax.toFixed(2);
+};
+
 const pizzaSales = () => {
   $.each(salesTaxData.provinces, (abbr, province) => {
-    $('#provinces').append(`<option>${province.name}</option>`);
+    const totalTax = calculateTotalTax(abbr);
+    $('#provinces').append(`<option value="${totalTax}">${province.name}</option>`);
   });
 
-  $('#provinces').click(function provinceClick() {
+  $('#provinces').change(function provinceClick() {
     // #4 in progress. Improve the event and add values for the province <options>
-    console.log('Province selected', this);
+    console.log('Province selected', $(this).val());
   });
 };
 
