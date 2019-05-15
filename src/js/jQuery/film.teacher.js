c0541b063de59edffe3a1dd86a69ba9a01c40aca
danactive <dan@vanarts.com>
2019-05-15 10:01:31 -0700

    Homework Solution

---
+++ src/js/jQuery/film.teacher.js
@@ -0,0 +1,46 @@
+const populateCharacterDropdown = (response) => {
+  $.each(response.movies[0].abridged_cast, (index, value) => {
+    const actorName = value.name;
+    const { characters } = value;
+
+    $.each(characters, (charIndex, charName) => {
+      const html = `<option value="${actorName}">${charName}</option>`;
+      $('#characters').append(html);
+    });
+  });
+};
+
+const getFilmData = () => {
+  $.ajax({
+    url: '../film/cowboy.json',
+    success: (response) => {
+      populateCharacterDropdown(response);
+    },
+    error: (x, xx, errorMessage) => {
+      console.log('AJAX error', errorMessage);
+    },
+  });
+};
+
+const displayActorName = (actorName) => {
+  $('#actor').html(actorName);
+};
+
+const attachFilmEvents = () => {
+  $('#characters').change(function characterChange() {
+    const actorName = $(this).val();
+    displayActorName(actorName);
+  });
+};
+
+const cowboyHomework = () => {
+  getFilmData();
+  attachFilmEvents();
+};
+
+// If Node.js then export as public
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    cowboyHomework,
+  };
+}
