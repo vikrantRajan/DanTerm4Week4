56cc550c3583b08434f980666cacc0e9c1bdc6b3
danactive <dan@vanarts.com>
2019-05-29 11:45:30 -0700

    Step 2 solution

--- src/js/jQuery/autocomplete.teacher.js
+++ src/js/jQuery/autocomplete.teacher.js
@@ -1,10 +1,21 @@
+const displayCountries = (response) => {
+  $.each(response.items, (x, country) => {
+    $('#suggestion').append(`<li>${country}</li>`);
+  });
+};
+
 const bindDomAutocomplete = () => {
   $('#country').keyup(function countryKey() {
     const keyword = $(this).val();
 
     // todo inclass #2 make an AJAX call to autocomplete backend service
     // and display the results in the DOM
-    $('#suggestion').html(`<li>${keyword}</li>`);
+
+
+    $.ajax({
+      url: '/api/autocomplete',
+      success: displayCountries,
+    });
   });
 };
 
