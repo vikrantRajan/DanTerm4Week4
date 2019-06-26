10b14d016b566f4c5ea990f99326a9d2ab8e3fef
danactive <dan@vanarts.com>
2019-06-26 10:51:25 -0700

    Flickr API browser XHR call

---
+++ src/js/flickr/api.teacher.js
@@ -0,0 +1,18 @@
+const getFlickrGeoPublicPhotos = () => {
+  $.ajax({
+    url: 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=fd827f1740b5ebd99218383bd0436e4d&tags=vancouver&format=json',
+    dataType: 'jsonp', // JSON with Padding. Whereas padding is the function name that Flickr is wrapping JSON in
+    jsonpCallback: 'jsonFlickrApi',
+    success: (response) => {
+      const html = response.photos.photo.map(item => `<img alt="${item.title}">`);
+      $('body').append(html);
+    },
+  });
+};
+
+// If Node.js then export as public
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    getFlickrGeoPublicPhotos,
+  };
+}
