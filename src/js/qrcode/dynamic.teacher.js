54041616065b93b9bdf82aae0900345dff02b509
danactive <dan@vanarts.com>
2019-06-26 09:24:18 -0700

    QR Code solution

--- src/js/qrcode/dynamic.teacher.js
+++ src/js/qrcode/dynamic.teacher.js
@@ -1,21 +1,21 @@
 /* global document */
 
+const getQrImageHtml = (rawLabel) => {
+  const encodedLabel = encodeURIComponent(rawLabel);
+  const serviceAddress = `https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=${encodedLabel}`;
+
+  // Weakness poor performance as the image is destroyed and re-created every key tap
+  // Improvement would be to wait for the keyword to be completed and then request image
+  // For example lodash/debounce
+  return `<img src="${serviceAddress}" alt="QR Code">`;
+};
+
 const createQrBlob = () => {
   const qrInput = document.getElementById('qr_source');
   const qrOutput = document.getElementById('qr_code');
 
-  const getImageHtml = (rawLabel) => {
-    const encodedLabel = encodeURIComponent(rawLabel);
-    const serviceAddress = `https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=${encodedLabel}`;
-
-    // Weakness poor performance as the image is destroyed and re-created every key tap
-    // Improvement would be to wait for the keyword to be completed and then request image
-    // For example lodash/debounce
-    return `<img src="${serviceAddress}" alt="QR Code">`;
-  };
-
   const updateQrCode = () => {
-    qrOutput.innerHTML = getImageHtml(qrInput.value);
+    qrOutput.innerHTML = getQrImageHtml(qrInput.value);
   };
 
   const bindDom = () => {
