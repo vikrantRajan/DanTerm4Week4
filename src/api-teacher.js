c66f1f14b3b4d952762c2a796e323941c7aeb0ea
danactive <dan@vanarts.com>
2019-06-26 11:59:28 -0700

    Moving code for readability

--- src/api-teacher.js
+++ src/api-teacher.js
@@ -11,67 +11,100 @@ const flickrJpgPaths = response => response.photos.photo
   .map(photo => `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`);
 exports.flickrJpgPaths = flickrJpgPaths;
 
+// keyword is a new variable, when undefined set to blank.
+// keyword is in an object with a parent property query
+const autocompleteHandler = ({ query: { keyword = '' } }) => {
+  // keyword is either blank or a string value
+  const DELAY = 1500; // 1.5 sec
+  const places = [];
+
+  places.push('Afghanistan');
+  places.push('Albania');
+  places.push('Algeria');
+  places.push('American Samoa');
+  places.push('Antarctica');
+  places.push('Argentina');
+  places.push('Armenia');
+  places.push('Aruba');
+  places.push('Australia');
+  places.push('Austria');
+  places.push('Bahamas');
+  places.push('Bangladesh');
+  places.push('Barbados');
+  places.push('Belarus');
+  places.push('Belgium');
+  places.push('Belize');
+  places.push('Bermuda');
+  places.push('Bolivia');
+  places.push('Brazil');
+  places.push('Bulgaria');
+  places.push('Cambodia');
+  places.push('Cameroon');
+  places.push('Canada');
+  places.push('Cayman Islands');
+  places.push('Chad');
+  places.push('Chile');
+  places.push('China');
+  places.push('Colombia');
+  places.push('Congo');
+  places.push('Cook Islands');
+  places.push('Costa Rica');
+  places.push('Côte d\'Ivoire');
+  places.push('Croatia');
+  places.push('Cuba');
+  places.push('Cyprus');
+  places.push('Czechia');
+
+  const items = places.filter(place => place.toLowerCase().includes(keyword.toLowerCase()));
+
+  if (items.length === 0) {
+    items.push('No matches found');
+  }
+
+  // I promise to return the value when setTimeout resolves with the answer
+  return new Promise(resolve => setTimeout(resolve, DELAY, { items }));
+};
+
+// Node.js rule, exports must in the root. Not inside a function or closure
+exports.autocompleteHandler = autocompleteHandler;
+
+const buildSlackMessage = (studentKey) => {
+  const student = course.assessment.students[studentKey];
+  const homeworkMark = student.homework;
+  const { latestNote } = student;
+  const additionalNote = (latestNote) ? `Additional notes: ${latestNote}` : '';
+
+  const message = `Hello ${studentKey}
+Class ${course.assessment.classNumber} - Homework mark update ${JSON.stringify(homeworkMark)}
+Your homework allocation is ${calculatePercent(homeworkMark)}%
+Documentation https://github.com/VanArts/course-files/tree/master/public/social-apis#assessment
+${additionalNote}`;
+
+  return message;
+};
+
+const sendSlackDirectMessage = slack => async ({ studentName }) => {
+  const student = course.assessment.students[studentName];
+  const message = buildSlackMessage(studentName);
+  const { error } = await slack.chat.postMessage({
+    text: message,
+    channel: student.slackUserId,
+  });
+
+  if (error) {
+    return {
+      error,
+      message: `Slack Direct Message failed to ${studentName}`,
+    };
+  }
+
+  return { message: `Slack Direct Message sent to ${studentName}:\n${message}` };
+};
+
 exports.plugin = {
   name: 'api',
   version: '1.3.0',
   register: (server) => {
-    // keyword is a new variable, when undefined set to blank.
-    // keyword is in an object with a parent property query
-    const autocompleteHandler = ({ query: { keyword = '' } }) => {
-      // keyword is either blank or a string value
-      const DELAY = 1500; // 1.5 sec
-      const places = [];
-
-      places.push('Afghanistan');
-      places.push('Albania');
-      places.push('Algeria');
-      places.push('American Samoa');
-      places.push('Antarctica');
-      places.push('Argentina');
-      places.push('Armenia');
-      places.push('Aruba');
-      places.push('Australia');
-      places.push('Austria');
-      places.push('Bahamas');
-      places.push('Bangladesh');
-      places.push('Barbados');
-      places.push('Belarus');
-      places.push('Belgium');
-      places.push('Belize');
-      places.push('Bermuda');
-      places.push('Bolivia');
-      places.push('Brazil');
-      places.push('Bulgaria');
-      places.push('Cambodia');
-      places.push('Cameroon');
-      places.push('Canada');
-      places.push('Cayman Islands');
-      places.push('Chad');
-      places.push('Chile');
-      places.push('China');
-      places.push('Colombia');
-      places.push('Congo');
-      places.push('Cook Islands');
-      places.push('Costa Rica');
-      places.push('Côte d\'Ivoire');
-      places.push('Croatia');
-      places.push('Cuba');
-      places.push('Cyprus');
-      places.push('Czechia');
-
-      const items = places.filter(place => place.toLowerCase().includes(keyword.toLowerCase()));
-
-      if (items.length === 0) {
-        items.push('No matches found');
-      }
-
-      // I promise to return the value when setTimeout resolves with the answer
-      return new Promise(resolve => setTimeout(resolve, DELAY, { items }));
-    };
-
-    // Node.js rule, exports must in the root. Not inside a function or closure
-    exports.autocompleteHandler = autocompleteHandler;
-
     server.route({
       method: 'GET',
       path: '/api/autocomplete',
@@ -150,39 +183,6 @@ exports.plugin = {
         };
       },
     });
-
-    const buildSlackMessage = (studentKey) => {
-      const student = course.assessment.students[studentKey];
-      const homeworkMark = student.homework;
-      const { latestNote } = student;
-      const additionalNote = (latestNote) ? `Additional notes: ${latestNote}` : '';
-
-      const message = `Hello ${studentKey}
-Class ${course.assessment.classNumber} - Homework mark update ${JSON.stringify(homeworkMark)}
-Your homework allocation is ${calculatePercent(homeworkMark)}%
-Documentation https://github.com/VanArts/course-files/tree/master/public/social-apis#assessment
-${additionalNote}`;
-
-      return message;
-    };
-
-    const sendSlackDirectMessage = slack => async ({ studentName }) => {
-      const student = course.assessment.students[studentName];
-      const message = buildSlackMessage(studentName);
-      const { error } = await slack.chat.postMessage({
-        text: message,
-        channel: student.slackUserId,
-      });
-
-      if (error) {
-        return {
-          error,
-          message: `Slack Direct Message failed to ${studentName}`,
-        };
-      }
-
-      return { message: `Slack Direct Message sent to ${studentName}:\n${message}` };
-    };
 
     server.route({
       method: 'GET',
