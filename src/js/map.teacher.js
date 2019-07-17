c01eca500c57a2e9b65951f516fdaafbde78742a
danactive <dan@vanarts.com>
2019-07-17 12:27:58 -0700

    Fix bug with mapbox accessToken

--- src/js/map.teacher.js
+++ src/js/map.teacher.js
@@ -1,8 +1,4 @@
 /* global mapboxgl */
-if (mapboxgl) {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuYWN0aXZlLXRlYWNoIiwiYSI6ImNqcmppNDlveDBjdHk0M284MnNya2Ztb2wifQ.lUbes1e5nrEYV9nwRh49sQ';
-}
-
 const vanarts = {
   latitude: 49.282733,
   longitude: -123.115433,
@@ -16,6 +12,10 @@ const createMap = () => new mapboxgl.Map({
 });
 
 const loadHelloMap = () => {
+  if (mapboxgl) {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuYWN0aXZlLXRlYWNoIiwiYSI6ImNqcmppNDlveDBjdHk0M284MnNya2Ztb2wifQ.lUbes1e5nrEYV9nwRh49sQ';
+  }
+
   const map = createMap();
 
   const bubble = new mapboxgl.Popup()
